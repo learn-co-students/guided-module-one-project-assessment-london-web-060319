7d92f0b0c5d2a2adc5e5f26d43ed09827c350997
Federica Macchi <federica.macchi3@gmail.com>
2019-06-18 17:56:50 +0100

    crud works

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,39 +1,58 @@
 class CommandLineInterface
     def run
         user = beginning
-        menu = ["What would you like to do?", "Write a review", "See all the books on our platform", "Edit a review","Delete a review"]
+        menu(user)
+    end    
+        
+
+    def menu(user)
+        menu = ["What would you like to do?", "Write a review", "See all books", "See all reviews", "Edit a review", "Delete a review"]
         puts_array(menu)
         choice = gets.chomp
         which_menu?(choice, menu, user)
-    end    
-        
-        def which_menu?(choice, menu, user)
-            if choice == menu[1]
-                create_review(user)
-                
-            elsif choice == menu[2]
-                puts_all_books    
-            
-            end
+    end
 
+    def which_menu?(choice, menu, user)
+        if choice == menu[1]
+            create_review(user)
+        elsif choice == menu[2]
+            puts_all_books   
+        elsif choice == menu[3]
+            puts_reviews(Review.all)        
+        elsif choice == menu[4]
+            puts "Which of your reviews would you like to edit?"
+            puts_reviews(user.reviews)
+            review_id = gets.chomp
+            puts "Please write your review below"
+            review_content = gets.chomp
+            review = Review.find_by(id: review_id)
+            review.update_attribute(:content, review_content)
+        elsif choice == menu[5]
+            puts "Please type the number of the review you would like to delete:"
+            puts_reviews(user.reviews)
+            review_id = gets.chomp
+            Review.delete(review_id)
+            puts "The review has been deleted!"
+        else
+            "Sorry, was that a typo?"
+            menu            
         end
-        # "Enter a book title to get started:"]
-        # puts_array(welcome_messages)
-        # book_title = gets.chomp 
-        # book = Book.find_by(title: book_title)
-        # book.reviews
+    end
+   
+
+
+
         # if book.reviews == []
         #     puts "No reviews for this one yet!"
         # else
         #     book.reviews
         # end
-        
-    
 
-    def puts_all_books
+       
+        def puts_all_books
         puts "This are all the books on our platform:"
         Book.all.each {|book| puts "#{book.id}. #{book.title}"}
-    end
+        end
 
     def beginning
         welcome_messages = ["Welcome to Fedsreads, the best book review platform in the world!",
@@ -51,29 +70,21 @@ class CommandLineInterface
     end
 
     def puts_reviews(reviews)
-        reviews.each {|review| puts "Review by #{review.user.username} - \"#{review.content}\""}
+        reviews.each {|review| puts "#{review.id}. Book: #{review.book.title}, User: #{review.user.username} - \"#{review.content}\""}
     end
 
     def create_review(user)
         puts "What book would you like to review?"
         puts_all_books
         book_title = gets.chomp
-        book = Book.find_by(title: book_title)
-        # binding.pry
-        
+        book = Book.find_by(title: book_title)        
         puts "Please write your review below"
-        review_content = gets.chomp
-    
+        review_content = gets.chomp    
         review = Review.create(content: review_content, book_id: book.id, user_id: user.id)
         puts "Successful!"
         book = Book.find_by(title: book_title)
         puts_reviews(book.reviews)
         
     end
-
-    def puts_book_reviews(book) # delete method if write works
-        book.reviews.each {|review| "Review @{book.id}.   "}
-    end
-
 
 end
