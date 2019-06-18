709e9f7c3501fee8a993be8a19c38ab2c8da4d32
Federica Macchi <federica.macchi3@gmail.com>
2019-06-18 23:33:33 +0100

    Switched to tty

--- bin/run.rb
+++ bin/run.rb
@@ -1,13 +1,8 @@
 require_relative '../config/environment'
 
-cli = CommandLineInterface.new
-reviews = cli.run
-# cli.puts_reviews(reviews)
-# show_all_books
-# cli.puts_reviews(reviews)
-
-# cli.create_review
-# cli.puts_reviews(reviews)
+welcome_msg
+user = this_user
+menu(user)
 
 
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,90 +1,107 @@
-class CommandLineInterface
-    def run
-        user = beginning
-        menu(user)
-    end    
-        
+$prompt = TTY::Prompt.new
 
-    def menu(user)
-        menu = ["What would you like to do?", "Write a review", "See all books", "See all reviews", "Edit a review", "Delete a review"]
-        puts_array(menu)
-        choice = gets.chomp
-        which_menu?(choice, menu, user)
+def welcome_msg
+    system 'clear'
+    $prompt.say("Welcome to Fedsreads, the best book review platform in the world!\nThinking of reading something but not sure what? We can help you with that!")
+end
+
+def this_user
+    username = $prompt.ask("What is your username?")
+    user = User.find_or_create_by(username: username)
+    return user
+end
+
+def failed(message, choice, user) 
+    system "clear"
+    $prompt.say("#{message}")
+    $prompt.ask("Press enter to continue", echo: false)
+    which_choice?(choice, user)
+end
+
+def success(message, user)
+    system "clear"
+    $prompt.say("#{message}")
+    $prompt.ask('Press enter to continue', echo: false)
+    menu(user)
+end
+
+def menu(user)
+    system "clear"
+    $prompt.say("Welcome #{user.username}!")
+    choice = $prompt.select("What would you like to do?") do |menu|
+        menu.choice "Write a review", 1
+        menu.choice "See all books", 2
+        menu.choice "See all reviews", 3
+        menu.choice "Edit a review", 4
+        menu.choice "Delete a review", 5
+        menu.choice "Exit"
     end
+    which_choice?(choice, user)
+end
 
-    def which_menu?(choice, menu, user)
-        if choice == menu[1]
-            create_review(user)
-        elsif choice == menu[2]
-            puts_all_books   
-        elsif choice == menu[3]
-            puts_reviews(Review.all)        
-        elsif choice == menu[4]
-            puts "Which of your reviews would you like to edit?"
-            puts_reviews(user.reviews)
-            review_id = gets.chomp
-            puts "Please write your review below"
-            review_content = gets.chomp
-            review = Review.find_by(id: review_id)
-            review.update_attribute(:content, review_content)
-        elsif choice == menu[5]
-            puts "Please type the number of the review you would like to delete:"
-            puts_reviews(user.reviews)
-            review_id = gets.chomp
-            Review.delete(review_id)
-            puts "The review has been deleted!"
-        else
-            "Sorry, was that a typo?"
-            menu            
-        end
-    end
-   
-
-
-
-        # if book.reviews == []
-        #     puts "No reviews for this one yet!"
-        # else
-        #     book.reviews
-        # end
-
-       
-        def puts_all_books
-        puts "This are all the books on our platform:"
-        Book.all.each {|book| puts "#{book.id}. #{book.title}"}
-        end
-
-    def beginning
-        welcome_messages = ["Welcome to Fedsreads, the best book review platform in the world!",
-        "Thinking of reading something but not sure what? We can help you with that!", 
-        "First of all, what is your username?"]
-        puts_array(welcome_messages)
-        username = gets.chomp
-        user = User.find_or_create_by(username: username)
-        puts "Welcome #{username}!"
-        return user
-    end
-
-    def puts_array(array)
-        array.each {|item| puts item}
-    end
-
-    def puts_reviews(reviews)
-        reviews.each {|review| puts "#{review.id}. Book: #{review.book.title}, User: #{review.user.username} - \"#{review.content}\""}
-    end
-
-    def create_review(user)
-        puts "What book would you like to review?"
+def which_choice?(choice, user)
+    system "clear"
+    if choice == 1
+        create_review(user)
+    elsif choice == 2 
         puts_all_books
-        book_title = gets.chomp
-        book = Book.find_by(title: book_title)        
-        puts "Please write your review below"
-        review_content = gets.chomp    
-        review = Review.create(content: review_content, book_id: book.id, user_id: user.id)
-        puts "Successful!"
-        book = Book.find_by(title: book_title)
-        puts_reviews(book.reviews)
-        
+        $prompt.ask("\nPress enter to go back to main menu", echo: false)
+        menu(user)
+    elsif choice == 3
+        puts_reviews(Review.all)
+        $prompt.ask("\nPress enter to go back to main menu", echo: false)
+        menu(user)
+    elsif choice == 4
+        edit_review(user)
+    elsif choice == 5
+        delete_review(user)
     end
+end
 
+def create_review(user)
+    puts_all_books
+    book_title = $prompt.ask("What book would you like to review?")
+    book = Book.find_or_create_by(title: book_title)
+    review_content = $prompt.ask("Please write your review here")
+    review = Review.create(content: review_content, book_id: book.id, user_id: user.id)
+    message = "Thank you! Your review was added to our collection!"
+    success(message, user)
+end
+
+def puts_all_books
+    $prompt.say("These are all the books on our platform: \n")
+    Book.all.each {|book| puts "#{book.id}. #{book.title}"}
+end
+
+def puts_reviews(reviews)
+    reviews.each {|review| puts "#{review.id}. Book: \"#{review.book.title}\", User: #{review.user.username} - #{review.content}"}
+end
+
+def edit_review(user)
+    puts_reviews(user.reviews)
+    review_id = $prompt.ask("Pick the number of the review you would like to edit")
+    review = Review.find_by(id: review_id)
+    if user.reviews.include? review
+        review_content = $prompt.ask("Please write your review")
+        review.update_attribute(:content, review_content)
+        message = "Your review is now updated."
+        success(message, user)
+    else
+        message = "You can't edit this review."
+        failed(message, 4, user)
+    end     
+end
+
+def delete_review(user)
+    puts_reviews(user.reviews)
+    review_id = $prompt.ask("Please type the number of the review you would like to delete:")
+    
+    if Review.find_by(id: review_id, user_id: user.id)
+        Review.delete(review_id)    
+        message = "Success! The review has been deleted!"
+        success(message, user)
+    else
+        message = "You can't delete this review."
+        failed(message, 5, user)
+    end
 end
